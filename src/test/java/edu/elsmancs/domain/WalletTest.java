bb7c3f4342782ace8a8551000a1df1c8b2e2dc11
samuFerragut <sferragut@cifpfbmoll.eu>
2020-03-01 21:49:08 +0100

    test(WalletTest): Añadir caso test generateKeyPair

--- src/test/java/edu/elsmancs/domain/WalletTest.java
+++ src/test/java/edu/elsmancs/domain/WalletTest.java
@@ -8,11 +8,20 @@ import java.security.KeyPair;
 public class WalletTest {
 
     @Test
-    public void setSKTest() {
+    public void getAddressTest() {
         Wallet wallet = new Wallet();
         KeyPair pair = GenSig.generateKeyPair();
         wallet.setAddress(pair.getPublic());
         assertEquals(wallet.getAddress().hashCode(), wallet.getAddress().hashCode());
         System.out.println(wallet.getAddress().hashCode());
+    }
+
+    @Test
+    public void generateKeyPairTest() {
+        Wallet wallet = new Wallet();
+        wallet.generateKeyPair();
+        assertEquals(wallet.getAddress().hashCode(), wallet.getAddress().hashCode());
+        System.out.println(wallet.getAddress().hashCode());
+        assertEquals(wallet.getSkey.hashCode(), wallet.getSkey.hashCode());
     }
 }
