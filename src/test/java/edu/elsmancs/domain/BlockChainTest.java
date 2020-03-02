b8c26211c8f60ae6fbc6dd84ac0a55146a089400
samuFerragut <sferragut@cifpfbmoll.eu>
2020-03-02 12:46:12 +0100

    test(BlockChainTest): Añadir casos test summarize

--- src/test/java/edu/elsmancs/domain/BlockChainTest.java
+++ src/test/java/edu/elsmancs/domain/BlockChainTest.java
@@ -14,4 +14,24 @@ public class BlockChainTest {
         blockChain.addOrigin(transaction);
         assertEquals(1, blockChain.getBlockChain().size());
     }
+
+    @Test
+    public void summarizeTest() {
+        BlockChain blockChain = new BlockChain();
+        Wallet wallet = new Wallet();
+        wallet.generateKeyPair();
+        Transaction transaction = new Transaction("hash_1", "0", wallet.getAddress(), wallet.getAddress(), 20, "a flying pig!");
+        blockChain.addOrigin(transaction);
+        blockChain.summarize();
+    }
+
+    @Test
+    public void summarizePositionTest() {
+        BlockChain blockChain = new BlockChain();
+        Wallet wallet = new Wallet();
+        wallet.generateKeyPair();
+        Transaction transaction = new Transaction("hash_1", "0", wallet.getAddress(), wallet.getAddress(), 20, "a flying pig!");
+        blockChain.addOrigin(transaction);
+        blockChain.summarize(0);
+    }
 }
