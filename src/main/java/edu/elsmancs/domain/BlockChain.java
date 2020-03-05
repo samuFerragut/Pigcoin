c8e1d424c7702a61a7835b591b1e51d2e0845b19
samuFerragut <sferragut@cifpfbmoll.eu>
2020-03-05 12:16:18 +0100

    feat(BlockChain): Crear processTransaction + métodos necesarios

--- src/main/java/edu/elsmancs/domain/BlockChain.java
+++ src/main/java/edu/elsmancs/domain/BlockChain.java
@@ -3,6 +3,7 @@ package edu.elsmancs.domain;
 import java.security.PublicKey;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class BlockChain {
@@ -45,5 +46,27 @@ public class BlockChain {
         List<Transaction> output = loadOutputTransactio(address);
         List[] inOutput = {input, output};
         return inOutput;
+    }
+
+    void processTransactions(PublicKey address, PublicKey pKey_recipient, Map<String, Double> consumedCoins, String message, byte[] signedTransaction) {
+        isSignatureValid(address, message, signedTransaction);
+        isConsumedCoinsValid(consumedCoins);
+        createTransaction(address, pKey_recipient, consumedCoins, message, signedTransaction);
+    }
+
+    private void createTransaction(PublicKey pKey_sender, PublicKey pKey_recipent, Map<String, Double> consumedCoins, String message, byte[] signedTransaction) {
+        Transaction transaction = new Transaction();
+        transaction.setpKey_sender(pKey_sender);
+        transaction.setpKey_recipient(pKey_recipent);
+        transaction.setMessage(message);
+        blockChain.add(transaction);
+    }
+
+    private void isConsumedCoinsValid(Map<String, Double> consumedCoins) {
+    }
+
+    private boolean isSignatureValid(PublicKey address, String message, byte[] signedTransaction) {
+        return GenSig.verify(address, message, signedTransaction);
+
     }
 }
